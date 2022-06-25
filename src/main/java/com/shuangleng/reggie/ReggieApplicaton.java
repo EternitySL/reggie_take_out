ec641f0e00b1f9ce41859a24cc59b6d383fccac3
张鹏 <707632915@qq.com>
2022-06-25 17:00:35 +0800

    Initial commit

--- src/main/java/com/shuangleng/reggie/ReggieApplicaton.java
+++ src/main/java/com/shuangleng/reggie/ReggieApplicaton.java
@@ -1,12 +1,9 @@
 package com.shuangleng.reggie;
 
 import com.shuangleng.reggie.controller.EmployeeController;
-import com.shuangleng.reggie.entity.Employee;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Slf4j
@@ -16,7 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class  ReggieApplicaton {
     private EmployeeController employeeController;
 
-    public static void main(String[] args) {
+    public static    void main(String[] args) {
         SpringApplication.run(ReggieApplicaton.class, args);
 
 
