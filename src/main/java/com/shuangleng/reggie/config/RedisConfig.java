cab05a528ffcf978b17b44e5059ac643ee15aba5
张鹏 <707632915@qq.com>
2022-07-01 14:18:17 +0800

    Initial commit

---
+++ src/main/java/com/shuangleng/reggie/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.shuangleng.reggie.config;
+
+/**
+ * @author ：shuangleng
+ * @date ：Created in 2022/7/1 14:06
+ * @description：序列化和反序列化map对象
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
